f6071c036279ea7799d74abeae22a14d43b27cc4
Michal Barth <u17.401@pslib.cloud>
2020-03-17 17:41:43 +0100

    Calculator 17.3.

--- Calculator/ViewModel/MainViewModel.cs
+++ Calculator/ViewModel/MainViewModel.cs
@@ -11,6 +11,10 @@ namespace Calculator.ViewModel
     class MainViewModel : INotifyPropertyChanged
     {
         private string _value;
+        private string _value1;
+        private string _value2;
+        private string _oper;
+        private int _result;
 
         public RelayCommand Reset { get; set; }
         public RelayCommand Point { get; set; }
@@ -35,6 +39,10 @@ namespace Calculator.ViewModel
                     {
                         Value = Value.Remove(Value.Length - 1, 1) + newChar;
                     }
+                    else if (Value.EndsWith(".0") && newChar == "0")
+                    {
+                        Value = "0" + newChar;
+                    }
                     else Value += newChar;
                 },
                 param => true
@@ -56,6 +64,121 @@ namespace Calculator.ViewModel
                 },
                 () => true
                 );
+            Reset = new RelayCommand(
+                () =>
+                {
+                    Value = "0";
+                    Point.RaiseCanExecuteChanged();
+                },
+                () => true
+                );
+            Operation = new ParametrizedRelayCommand(
+                param =>
+                {
+                    Point.RaiseCanExecuteChanged();
+                    switch (param)
+                    {
+                        case "+":
+                            Value1 = Value;
+                            Value = "0";
+                            Oper = "+";
+                            break;
+                        case "-":
+                            Value1 = Value;
+                            Value = "0";
+                            Oper = "-";
+                            break;
+                        case "*":
+                            Value1 = Value;
+                            Value = "0";
+                            Oper = "*";
+                            break;
+                        case "/":
+                            Value1 = Value;
+                            Value = "0";
+                            Oper = "/";
+                            break;
+                        case "^":
+                            Value1 = Value;
+                            Value = "0";
+                            Oper = "^";
+                            break;
+                        case "√x":
+                            Value1 = Value;
+                            Oper = "√x";
+                            break;
+                        case "sin(x)":
+                            Value1 = Value;
+                            Oper = "sin(x)";
+                            break;
+                        case "cos(x)":
+                            Value1 = Value;
+                            Oper = "cos(x)";
+                            break;
+
+                        case "=":
+                            Value2 = Value;
+                            double x = Double.Parse(Value1);
+                            double y = Double.Parse(Value2);
+                            double result;
+                            switch (Oper)
+                            {
+                                case "+":
+                                    result = x + y;
+                                    Value = result.ToString();
+                                    break;
+                                case "-":
+                                    result = x - y;
+                                    Value = result.ToString();
+                                    break;
+                                case "*":
+                                    result = x * y;
+                                    Value = result.ToString();
+                                    break;
+                                case "/":
+                                    if (y == 0)
+                                    {
+                                        Value = "Dělení nulou";
+                                    }
+                                    else
+                                    {
+                                        result = x / y;
+                                        Value = result.ToString();
+                                    }
+                                    break;
+                                case "^":
+                                    result = Math.Pow(x, y);
+                                    Value = result.ToString();
+                                    break;
+                                case "√x":
+                                    if (x < 0)
+                                    {
+                                        Value = "Odmocnění záporného čísla";
+                                    }
+                                    else
+                                    {
+                                        result = Math.Sqrt(x);
+                                        Value = result.ToString();
+                                    }
+                                    break;
+                                case "sin(x)":
+                                    result = Math.Sin(x);
+                                    Value = result.ToString();
+                                    break;
+                                case "cos(x)":
+                                    result = Math.Cos(x);
+                                    Value = result.ToString();
+                                    break;
+                                default:
+                                    break;
+                            }
+                            break;
+                        default:
+                            break;
+                    }
+                },
+                param => true
+                );
         }
 
         public string Value
@@ -67,6 +190,57 @@ namespace Calculator.ViewModel
             set
             {
                 _value = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        public string Value1
+        {
+            get
+            {
+                return _value1;
+            }
+            set
+            {
+                _value1 = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        public string Value2
+        {
+            get
+            {
+                return _value2;
+            }
+            set
+            {
+                _value2 = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        public string Oper
+        {
+            get
+            {
+                return _oper;
+            }
+            set
+            {
+                _oper = value;
+                NotifyPropertyChanged();
+            }
+        }
+        public int Result
+        {
+            get
+            {
+                return _result;
+            }
+            set
+            {
+                _result = value;
                 NotifyPropertyChanged();
             }
         }
